b9199a3386522eda088885d4619456515f6bf8cb
Alexandr_Mazan <alexandrmazanik@gmail.com>
2022-11-23 00:54:12 +0300

    samples are creating

--- sf_boson_sampler.py
+++ sf_boson_sampler.py
@@ -30,19 +30,16 @@ def boson_sampling(modes_num, injected_photons_num, batch_size):
 
         MeasureFock() | q
 
-    eng = sf.Engine(backend="fock", backend_options={"cutoff_dim": 3})
+    eng = sf.Engine(backend="fock", backend_options={"cutoff_dim": injected_photons_num + 1})
 
     results = []
-    j = 0
     for i in range(batch_size):
         results.append(eng.run(boson_sampler))
-        if results[i].samples[0][1] == 2:
-            j += 1
-    print("prob = ", j / batch_size)
+        print(results[i].samples)
 
 
 def main():
-    boson_sampling(modes_num=2, injected_photons_num=2, batch_size=1000)
+    boson_sampling(modes_num=4, injected_photons_num=4, batch_size=10)
 
 
 if __name__ == '__main__':
