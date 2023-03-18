8fad4492c3f99e0c62c5390f3810f6043960cae6
Alexandr_Mazan <alexandrmazanik@gmail.com>
2023-03-19 01:43:25 +0300

    reading of a block scheme added

--- block_boson_sampler.py
+++ block_boson_sampler.py
@@ -25,49 +25,56 @@ class BeamSplitter:
         self.bs_matrix = sparse.csr_matrix((data, (row_ind, col_ing)), shape=(number_of_modes, number_of_modes))
 
 
-class Scheme:
-    def __init__(self, modes_num=1):
+class Block:
+    def __init__(self, modes_num):
         self.number_of_modes = modes_num
         self._beam_splitters = []
-        self.scheme_matrix = sparse.csr_matrix(np.identity(self.number_of_modes))
+        self.block_matrix = sparse.csr_matrix(np.identity(self.number_of_modes))
 
     def add_BS_gate(self, modes, theta=np.pi/4, phi_rho=0., phi_tau=0.):
         bs = BeamSplitter(theta, phi_rho, phi_tau)
         bs.calc_bs_matrix(self.number_of_modes, modes)
         self._beam_splitters.append(bs)
 
-    def calc_scheme_matrix(self):
+    def calc_block_matrix(self):
         time_unit_start = time.time()
-        self.scheme_matrix = sparse.csr_matrix(np.identity(self.number_of_modes))
+        self.block_matrix = sparse.csr_matrix(np.identity(self.number_of_modes))
         for bs in np.flip(self._beam_splitters):
-            self.scheme_matrix = sparse.csr_matrix.dot(self.scheme_matrix, bs.bs_matrix)
-        self.scheme_matrix = self.scheme_matrix.toarray()
+            self.block_matrix = sparse.csr_matrix.dot(self.block_matrix, bs.bs_matrix)
+        self.block_matrix = self.block_matrix.toarray()
         time_unit_end = time.time()
         print("--> The time for dot product is :", (time_unit_end - time_unit_start) * 10 ** 3, "ms")
 
-    def upload_scheme_from_file(self, file_name):
+
+class Scheme:
+    def __init__(self):
+        self._blocks = []
+
+    def upload_scheme_from_file(self, file_name='curr_scheme.txt'):
+        ref_mode = 1
         with open(os.path.join('scheme', file_name), 'r') as f_scheme:
-            self.number_of_modes = int(f_scheme.readline())
-            for f_beam_splitter in f_scheme:
-                mode1, mode2 = list(map(int, f_beam_splitter.split('\t')[0:2]))
-                theta, phi_rho, phi_tau = list(map(float, f_beam_splitter.split('\t')[2:]))
-                self.add_BS_gate((mode1, mode2), theta, phi_rho, phi_tau)
+            for f_line in f_scheme:
+                if f_line.strip() == "BLOCK":
+                    modes_num, repetitions_num = list(map(int, f_scheme.readline().split()))
+                    block = Block(modes_num)
+                    for f_beam_splitter in f_scheme:
+                        if f_beam_splitter.strip() == "END":
+                            block.calc_block_matrix()
+                            for _ in range(repetitions_num):
+                                self._blocks.append({'block': block,
+                                                     'ref_mode': ref_mode})
+                                ref_mode += 1
+                            break
+                        mode1, mode2 = list(map(int, f_beam_splitter.split('\t')[0:2]))
+                        theta, phi_rho, phi_tau = list(map(float, f_beam_splitter.split('\t')[2:]))
+                        block.add_BS_gate((mode1, mode2), theta, phi_rho, phi_tau)
+
         print("--> Scheme was successfully uploaded")
-        print("--> Number of modes: ", self.number_of_modes)
 
-    def export_scheme_matrix(self, file_name):
-        with open(os.path.join('scheme', file_name), 'w') as f_out:
-            for line in np.round(self.scheme_matrix, 6):
-                for element in line:
-                    f_out.write(str(element.real) + '\t' + str(element.imag) + '\t')
-                f_out.write('\n')
-        print("--> Unitary was successfully exported")
-
-    def print_scheme_matrix(self):
-        print("--> U:\n", np.round(self.scheme_matrix, 3))
 
 def main():
-    print("Hello")
+    scheme = Scheme()
+    scheme.upload_scheme_from_file()
 
 
 if __name__ == '__main__':
